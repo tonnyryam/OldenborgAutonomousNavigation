6cd40c4ca8653972488d5ec42c31888cd4f7871f
Kellie Au <kelliejau3224@gmail.com>
2024-06-13 09:36:18 -0700

    Create runnable command line package for ue5osc

---
+++ ue5osc/ue5osc/__main__.py
@@ -0,0 +1,47 @@
+from argparse import ArgumentParser
+
+from . import Communicator
+
+
+def main():
+    """Argument Parser that verifies that the image path is getting passed in and
+    optional ability to set ip and ports."""
+    parser = ArgumentParser()
+    parser.add_argument("--ip", type=str, default="127.0.0.1", help="IP Address")
+    parser.add_argument("--ue_port", type=int, default=7447, help="UE server port.")
+    parser.add_argument("--py_port", type=int, default=7001, help="Python server port.")
+    # parser.add_argument("--resolution", type=str, default="1280x720", help="Resolution")
+    # parser.add_argument("--save_image", type=str, help="Image path")
+    parser.add_argument("--reset", action="store_true", help="Reset to start position")
+    parser.add_argument(
+        "--set_location",
+        type=float,
+        nargs=2,
+        help="Set (x,y) location of Unreal Camera",
+    )
+    parser.add_argument(
+        "--set_yaw", type=float, help="Set the rotation (on xy-plane) of Unreal Camera"
+    )
+    parser.add_argument(
+        "--get_location", help="Return current location of Unreal Camera"
+    )
+
+    args = parser.parse_args()
+
+    with Communicator(args.ip, args.ue_port, args.py_port) as osc_communicator:
+        if args.reset:
+            osc_communicator.reset()
+
+        if args.set_location:
+            osc_communicator.set_location_xy(args.set_location[0], args.set_location[1])
+
+        if args.set_yaw:
+            osc_communicator.set_yaw(args.set_yaw)
+
+        if args.get_location:
+            osc_communicator.get_location()
+
+
+# Calling main function
+if __name__ == "__main__":
+    main()
