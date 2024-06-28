8f23a50d7b3d27dcc726373c6d1ca786dd4bb380
Kellie Au <kelliejau3224@gmail.com>
2024-06-28 16:48:22 -0700

    Add an edited oldenborg_boxes so Wandering/Teleport cannot see outside (while being "in bounds")

--- boxnav/boxnav/environments.py
+++ boxnav/boxnav/environments.py
@@ -4,7 +4,24 @@ oldenborg_boxes = [
     aligned_box(left=4640, right=5240, lower=110, upper=1510, target=(4940, 870)),
     aligned_box(left=3720, right=5240, lower=700, upper=1040, target=(4000, 870)),
     aligned_box(left=3850, right=4120, lower=360, upper=1040, target=(4000, 400)),
-    aligned_box(left=110, right=4120, lower=260, upper=540, target=(255, 400)),
+    aligned_box(left=110, right=4120, lower=315, upper=540, target=(255, 400)),
+    aligned_box(left=150, right=400, lower=-1980, upper=540, target=(255, -1850)),
+    aligned_box(left=-1550, right=400, lower=-1980, upper=-1720, target=(-825, -1850)),
+    aligned_box(left=-900, right=-700, lower=-1980, upper=3320, target=(-825, 2485)),
+    aligned_box(left=-900, right=230, lower=2150, upper=2820, target=(150, 2485)),
+]
+
+oldenborg_boxes_edited = [
+    aligned_box(left=4640, right=5240, lower=110, upper=1510, target=(4940, 870)),
+    aligned_box(
+        left=3720, right=5240, lower=700, upper=1030, target=(4000, 865)
+    ),  # Changed upper and target
+    aligned_box(
+        left=3850, right=4120, lower=360, upper=1030, target=(4000, 400)
+    ),  # Changed upper
+    aligned_box(
+        left=110, right=4120, lower=345, upper=540, target=(255, 440)
+    ),  # Changed lower and target
     aligned_box(left=150, right=400, lower=-1980, upper=540, target=(255, -1850)),
     aligned_box(left=-1550, right=400, lower=-1980, upper=-1720, target=(-825, -1850)),
     aligned_box(left=-900, right=-700, lower=-1980, upper=3320, target=(-825, 2485)),
