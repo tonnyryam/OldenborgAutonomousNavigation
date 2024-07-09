d863e6de7daa16cd68c28725f3d7eb3fa5ace97b
Kellie Au <kelliejau3224@gmail.com>
2024-07-09 09:30:04 -0700

    srun not working in training script

--- learning/train_wandering_resnet18.sh
+++ learning/train_wandering_resnet18.sh
@@ -29,9 +29,9 @@ module load miniconda3
 conda activate s24 
 
 # Run training script
-srun --nodes=1 --ntask=1 --exclusive python training.py WanderingStaticModel Summer2024Official "Training Model on Wandering Static Data with ResNet18" ResNet18 Wandering100kData  
-srun --nodes=1 --ntask=1 --exclusive python training.py WanderingRand10Model Summer2024Official "Training Model on Wandering Randomized Textures every 10 Data with ResNet18" ResNet18 Wandering100kRandEvery10Data
-srun --nodes=1 --ntask=1 --exclusive python training.py WanderingRand50Model Summer2024Official "Training Model on Wandering Randomized Textures every 50 Data  with ResNet18" ResNet18 Wandering100kRandEvery50Data
+python training.py WanderingStaticModel Summer2024Official "Training Model on Wandering Static Data with ResNet18" ResNet18 Wandering100kData  
+python training.py WanderingRand10Model Summer2024Official "Training Model on Wandering Randomized Textures every 10 Data with ResNet18" ResNet18 Wandering100kRandEvery10Data
+python training.py WanderingRand50Model Summer2024Official "Training Model on Wandering Randomized Textures every 50 Data  with ResNet18" ResNet18 Wandering100kRandEvery50Data
 
 # Print the date again to see how long the job took
 date
