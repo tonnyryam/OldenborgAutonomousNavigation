6f7060c095c20393147bade25ccac345de43c67e
Kellie Au <kelliejau3224@gmail.com>
2024-07-24 10:41:25 -0700

    rename directory to be more appropriate for including the datasets (on the server)

--- learning/training_items/train_combined_resnet18.sh
+++ learning/training_items/train_combined_resnet18.sh
@@ -29,9 +29,9 @@ module load miniconda3
 conda activate s24 
 
 # Run training script
-srun --nodes=1 --ntasks=1 --exclusive python ./../../../training.py CombinedStaticResNet18 Summer2024Official "Training Model on All Static Datasets with ResNet18" ResNet18 Perfect100kData Wandering100kData Teleporting100kData --local_data
-srun --nodes=1 --ntasks=1 --exclusive python ./../../../training.py CombinedRand10ResNet18 Summer2024Official "Training Model on All Randomized Textures every 10 Datasets with ResNet18" ResNet18 Perfect100kRandEvery10Data Wandering100kRandEvery10Data Teleporting100kRandEvery10Data --local_data
-srun --nodes=1 --ntasks=1 --exclusive python ./../../../training.py CombinedRand50ResNet18 Summer2024Official "Training Model on All Randomized Textures every 50 Datasets with ResNet18" ResNet18 Perfect100kRandEvery50Data Wandering100kRandEvery50Data Teleporting100kRandEvery50Data --local_data
+srun --nodes=1 --ntasks=1 --exclusive python ./../training.py CombinedStaticResNet18 Summer2024Official "Training Model on All Static Datasets with ResNet18" ResNet18 Perfect100kData Wandering100kData Teleporting100kData --local_data
+srun --nodes=1 --ntasks=1 --exclusive python ./../training.py CombinedRand10ResNet18 Summer2024Official "Training Model on All Randomized Textures every 10 Datasets with ResNet18" ResNet18 Perfect100kRandEvery10Data Wandering100kRandEvery10Data Teleporting100kRandEvery10Data --local_data
+srun --nodes=1 --ntasks=1 --exclusive python ./../training.py CombinedRand50ResNet18 Summer2024Official "Training Model on All Randomized Textures every 50 Datasets with ResNet18" ResNet18 Perfect100kRandEvery50Data Wandering100kRandEvery50Data Teleporting100kRandEvery50Data --local_data
 
 # Print the date again to see how long the job took
 date
